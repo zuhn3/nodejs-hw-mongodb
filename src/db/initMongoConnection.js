4e1dc418c25b4b88d1963f7435647ab06019d6ea
zuhn3 <aop1603@gmail.com>
2025-10-22 17:01:22 +0100

    hw2 changes

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,14 +5,13 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 export const initMongoConnection = async () => {
   try {
     const user = getEnvVar('MONGODB_USER');
-    const pwd = encodeURIComponent(getEnvVar('MONGODB_PASSWORD'));
+    const pwd = getEnvVar('MONGODB_PASSWORD');
     const url = getEnvVar('MONGODB_URL');
     const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
-  `mongodb+srv://${user}:${pwd}@${url}/${db}` +
-  `?retryWrites=true&w=majority&authSource=admin&authMechanism=SCRAM-SHA-256`
-);
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+    );
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
